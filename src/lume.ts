6a830040d8967f9e1b029cd15c03d5b1ae0550c1
Owen Buckley <owenbuckley13@gmail.com>
2025-10-03 20:18:27 -0400

    initial hello world example

--- src/lume.ts
+++ src/lume.ts
@@ -1,5 +1,22 @@
-import { defineElements } from 'lume';
+import 'lume';
 
-console.log('Lume is loaded.');
+// TODO: get full demo working
+// TODO: get full type-checking setup
 
-defineElements();
+// https://docs.lume.io/examples/hello-world/
+let lastTime = performance.now()
+let dt = 0
+// @ts-expect-error
+moonRotator.rotation = (x, y, z, time) => {
+  dt = time - lastTime
+  lastTime = time
+  return [x, y, z + dt * 0.01]
+}
+
+// ^ We could've written it more simply but then rotation would start at an angle
+// based on the current time instead of our preferred starting angle:
+// moonRotator.rotation = (x, y, z, t) => [x, y, t * 0.004];
+// @ts-expect-error
+earth.rotation = (x, y, z, t) => [x, t * 0.01, z]
+// @ts-expect-error
+clouds.rotation = (x, y, z, t) => [x, -t * 0.003, z]
