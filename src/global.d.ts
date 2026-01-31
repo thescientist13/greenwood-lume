4ed73e7d9cb64826d9a21b2b849b860405b6c648
Owen Buckley <owenbuckley13@gmail.com>
2026-01-30 20:36:35 -0500

    feat: fix Lume types (#6)

---
+++ src/global.d.ts
@@ -0,0 +1,7 @@
+import type { Element3D } from 'lume';
+
+declare global {
+  var moonRotator: Element3D;
+  var earth: Element3D;
+  var clouds: Element3D;
+}
--- src/lume.ts
+++ src/lume.ts
@@ -1,11 +1,9 @@
 import 'lume';
 
-// TODO: get full type-checking setup
-
 // https://docs.lume.io/examples/hello-world/
 let lastTime = performance.now()
 let dt = 0
-// @ts-expect-error
+
 moonRotator.rotation = (x, y, z, time) => {
   dt = time - lastTime
   lastTime = time
@@ -15,7 +13,6 @@ moonRotator.rotation = (x, y, z, time) => {
 // ^ We could've written it more simply but then rotation would start at an angle
 // based on the current time instead of our preferred starting angle:
 // moonRotator.rotation = (x, y, z, t) => [x, y, t * 0.004];
-// @ts-expect-error
 earth.rotation = (x, y, z, t) => [x, t * 0.01, z]
-// @ts-expect-error
+
 clouds.rotation = (x, y, z, t) => [x, -t * 0.003, z]
